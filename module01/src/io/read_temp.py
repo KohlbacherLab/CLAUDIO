5a1dae4c753cbe806b0bcf18ae10523e0c3291e4
Nightknight3000 <alexander.roehl@uni-tuebingen.de>
2023-06-19 14:36:53 +0200

    Fix missing temppath

--- module01/src/io/read_temp.py
+++ module01/src/io/read_temp.py
@@ -2,17 +2,13 @@ import os
 import pandas as pd
 
 
-def read_temp_search_save(data, input_temppath, filename):
+def read_temp_search_save(data, uniprot_search_temp_dir, filename):
     # read temporary save file containing earlier uniprot search results, for quick reruns
     #
     # input data: pd.DataFrame, input_temppath: str, filename: str
     # return read_temp_search_save: pd.DataFrame
 
-    if input_temppath == '/':
-        project_path = '/'.join(os.path.abspath(__file__).split('/')[:-4])
-        project_path = project_path + '/' if project_path else ""
-        input_temppath = project_path
-    tmp_filepath = f"{input_temppath}uniprot_search/{filename}_srtmp.csv"
+    tmp_filepath = f"{uniprot_search_temp_dir}{filename}_srtmp.csv"
     tmp_data = pd.read_csv(tmp_filepath)
 
     return pd.concat([data, tmp_data], axis=1)
